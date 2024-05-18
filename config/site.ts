36481ec0f2d50b017302fb92a297403f74aed197
Shoaib Hossain <80954453+hossain101@users.noreply.github.com>
2024-05-18 13:20:30 +0600

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,69 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Shoaib's Portfolio",
+	description: "A result driven, self-motivated and resourceful software engineer with a proven ability to develop and strengthen teams in order to maximise company profitability and efficiency. Experienced leading and managing teams and projects successfully through various stages of development.",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+    {
+      label: "Docs",
+      href: "/docs",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    }
+	],
+	navMenuItems: [
+		{
+			label: "Profile",
+			href: "/profile",
+		},
+		{
+			label: "Dashboard",
+			href: "/dashboard",
+		},
+		{
+			label: "Projects",
+			href: "/projects",
+		},
+		{
+			label: "Team",
+			href: "/team",
+		},
+		{
+			label: "Calendar",
+			href: "/calendar",
+		},
+		{
+			label: "Settings",
+			href: "/settings",
+		},
+		{
+			label: "Help & Feedback",
+			href: "/help-feedback",
+		},
+		{
+			label: "Logout",
+			href: "/logout",
+		},
+	],
+	links: {
+		github: "https://github.com/hossain101",
+		twitter: "https://github.com/hossain101",
+		docs: "https://github.com/hossain101",
+		discord: "https://github.com/hossain101",
+    
+	},
+};
