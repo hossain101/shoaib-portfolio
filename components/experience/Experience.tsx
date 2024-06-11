c7cab5233c964a745bee5667ec3580054b99c3bc
Shoaib Hossain <80954453+hossain101@users.noreply.github.com>
2024-06-11 09:29:14 +0600

    refactor: Update Experience component for better layout and styling

--- components/experience/Experience.tsx
+++ components/experience/Experience.tsx
@@ -6,13 +6,15 @@ import FullExperience from "./FullExperience";
 
 const Experience = () => {
   return (
-    <section id="experience" className="min-h-full flex flex-col items-center   md:py-10">
-      <div className="min-w-full  min-h-screen flex flex-col items-center   z-[0] absolute bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,transparent_59px,#4f4f4f2e_1px)] bg-[size:60px_60px] pb-5 ">
-        <HeroHighlight />
-        <ExperienceTitle />
-        <ExperienceGrid />
-        <FullExperience />
-      </div>
+    <section
+      id="experience"
+      className="min-h-screen min-w-full flex flex-col items-center justify-center z-[0] relative bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,transparent_59px,#4f4f4f2e_1px)] bg-[size:60px_60px] pb-5 "
+    >
+      <HeroHighlight />
+
+      <ExperienceTitle />
+      <ExperienceGrid />
+      <FullExperience />
     </section>
   );
 };
